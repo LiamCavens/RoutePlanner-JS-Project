401bfc2a03f2c40606fa05ce979a7863d4f66504
Liam Cavens <liamcavens@googlemail.com>
2018-06-29 18:02:04 +0100

    added collage to front

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CollageBox from "./CollageBox";
 
 const Home = () => (
     <div>
         <h4>Home</h4>
         <p>Welcome to visit scotland chaps and female chaps</p>
+        <CollageBox />
     </div>
 );
 
