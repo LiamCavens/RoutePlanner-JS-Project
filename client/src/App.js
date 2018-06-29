f168ff69720712273922687d5d077b0572107121
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-06-29 14:40:06 +0100

    adds leaflet-react map wuth errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import Map from './components/Map.js'
+import "./App.css"
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Map />
       </div>
     );
   }
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import L from 'leaflet';
+import './Map.css';
+require('os-leaflet');
+
+
 class Map extends React.Component {
+
+
   componentDidMount() {
-    // create map
+
     this.map = L.map('map', {
-      center: [49.8419, 24.0315],
-      zoom: 16,
+      center: [55.8642, 4.2518],
+      zoom: 10,
       layers: [
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -13,8 +19,31 @@ class Map extends React.Component {
       ]
     });
   }
-  render() {
-    return <div id="map"></div>
+  // componentDidMount() {
+  //   this.layer = L.layerGroup().addTo(this.map);
+  //   this.updateMarkers(this.props.markersData);
+  // }
+
+
+componentWillReceiveProps({ markersData }) {
+
+  if (this.props.markersData !== markersData) {
+    this.updateMarkers(markersData);
   }
 }
+updateMarkers(markersData) {
+  this.layer.clearLayers();
+  markersData.forEach(marker => {
+    L.marker(
+      marker.latLng,
+      { title: marker.title }
+    ).addTo(this.layer);
+  });
+}
+
+render() {
+  return <div id="map" ></div>
+}
+}
+
 export default Map;
