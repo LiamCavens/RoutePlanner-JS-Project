ec2f21b8c045ac296e81cc9b2df58fd438e786ff
Liam Cavens <liamcavens@googlemail.com>
2018-06-29 14:15:09 +0100

    got some basic files up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import NavBar from './components/NavBar'
+import Main from './components/Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <Main />
       </div>
     );
   }
