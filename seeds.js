08c98533a18c710a2ed68d20ed81b4f94cb236a8
Liam Cavens <liamcavens@googlemail.com>
2018-06-29 13:09:14 +0100

    initial setup/commit

---
+++ seeds.js
@@ -0,0 +1,7 @@
+use routesDB;
+
+db.routes.remove({});
+
+db.routes.insert([
+  { quote: "Beings of light are we. Not this crude matter.", author: "Yoda" },
+]);
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+
+const port = 3000;
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(
+  "mongodb://localhost:27017",
+  (err, client) => {
+    if (err) next(err);
+    const db = client.db("routesDB");
+    console.log("Connected to db...Yaldi!!!");
+
+    app.get("/api/routes", (req, res, next) => {
+      const routesCollection = db.collection("routes");
+      routesCollection.find().toArray((err, routes) => {
+        if (err) next(err);
+        res.json(routes);
+      });
+    });
+
+    app.post("/api/routes", (req, res, next) => {
+      const routesCollection = db.collection("routes");
+      console.log(req.body);
+      const newRoute = req.body;
+      routesCollection.insert(newRoute, (err, result) => {
+        if (err) next(err);
+        res.status(201);
+        res.json(result.ops[0]);
+      });
+    });
+
+    app.listen(port, () => {
+      console.log("App listening on port", port);
+    });
+  }
+);
