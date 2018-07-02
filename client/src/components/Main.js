015b7b823a116ad4949dea82da68296a53436862
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-07-02 15:48:03 +0100

    adds link and page for westhighland way

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import About from './About';
 import RoutePlanner from './RoutePlanner';
 import UserPage from '../containers/UserPage';
+import westHighlandWay from './westHighlandWay'
 
 class Main extends Component {
     constructor(props) {
@@ -24,6 +25,7 @@ class Main extends Component {
                     <Route path='/about' component={About} />
                     <Route path='/users' component={UserPage} />
                     <Route path='/route-planner' component={RoutePlanner} />
+                    <Route path='/west-highland-way' component={westHighlandWay}/>
 
 
                 </React.Fragment>
---
+++ client/src/components/westHighlandWay.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./westHighlandWay.css";
+import TextBox from "./TextBox.js"
+
+const westHighlandWay = () => (
+  <div>
+    <h4 id="Route-header">West Highland Way</h4>
+    <div id="Route-container">
+      <h5 id="Route-sub-head">
+        Helping you to discover and explore the West Highland way
+      </h5>
+      <TextBox />
+
+    </div>
+  </div>
+);
+
+export default westHighlandWay;
