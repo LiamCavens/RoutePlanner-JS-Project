e2b64fcd918ea07143149ec35f6f3de1277bf230
Paul Kelly <paul.kelly20@gmail.com>
2018-07-03 10:42:25 +0100

    Can add new User

--- client/src/containers/UserPage.js
+++ client/src/containers/UserPage.js
@@ -8,7 +8,32 @@ export default class UserPage extends Component {
     super(props);
     this.state= {
       users: [],
+      newUserName: ""
     }
+    this.handleNewUser = this.handleNewUser.bind(this);
+    this.newUserName = this.newUserName.bind(this);
+  }
+
+  handleNewUser = (event) => {
+    event.preventDefault()
+    const request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:3001/api/users");
+    request.setRequestHeader("content-type", "application/json")
+    request.addEventListener("load", function(){
+      if(this.status !== 201) return;
+      const responseBody = JSON.parse(this.response);
+    })
+    let newUsersname = this.state.newUserName;
+    request.send(JSON.stringify({name: newUsersname, routes: []}));
+
+     window.location.reload();
+
+  }
+
+  newUserName = (event) => {
+    event.preventDefault()
+    this.setState({newUserName: event.target.value})
+
   }
 
   componentDidMount(){
@@ -23,6 +48,10 @@ export default class UserPage extends Component {
     return(
       <div >
           <h4>Users</h4>
+          <form onSubmit={this.handleNewUser}>
+            <input type="text" placeholder="Username" onChange={this.newUserName}/>
+            <input type="submit" value="New users" />
+          </form>
         <UserList users= {this.state.users}/>
 
       </div>
