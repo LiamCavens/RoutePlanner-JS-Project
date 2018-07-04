0f9c43c6cc3362db5c62fbf4b47e501d740c382e
Paul Kelly <paul.kelly20@gmail.com>
2018-07-04 09:29:22 +0100

    Alert happens when new route is saved

--- client/src/components/RoutePlanner.js
+++ client/src/components/RoutePlanner.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import RouteList from './RouteList';
-import UserList from './UserList';
 import L from 'leaflet';
 import './Map.css';
 import './route-planner.css'
@@ -27,8 +26,8 @@ export default class RoutePlanner extends Component {
       endTown:'',
       startLatLong: [],
       endLatLong: [],
-      method: "",
-      usersRoute: "glasgow"
+      method: "car",
+      usersRoute: ""
     }
 
     this.mainMap = this.mainMap.bind(this)
@@ -97,26 +96,24 @@ export default class RoutePlanner extends Component {
     request.addEventListener('load', () => {
       if(request.status !== 200) return;
       let startTown = JSON.parse(request.response);
-       this.state.startLatLong = [startTown.latt, startTown.longt];
+       this.setState({startLatLong:[startTown.latt, startTown.longt]})
 
     })
     request.send()
 
     let endUrl = `https://geocode.xyz/${endPoint}-uk?json=1`
-    console.log(endUrl);
     const request2 = new XMLHttpRequest()
     request2.open("GET", endUrl);
     request2.addEventListener('load', () => {
       if(request2.status !== 200) return;
       let endTown = JSON.parse(request2.response);
-       this.state.endLatLong = [endTown.latt, endTown.longt];
+      this.setState({endLatLong:[endTown.latt, endTown.longt]})
     })
     request2.send()
 
     setTimeout(() => {
-      console.log(this.newRoute);
       this.newRoute(this.state.startLatLong , this.state.endLatLong, this.state.method, this.state.startTown, this.state.endTown);
-    }, 6000);
+    }, 2000);
   }
 
   onStartTownChange(event) {
@@ -126,8 +123,6 @@ export default class RoutePlanner extends Component {
     this.setState({endTown: event.target.value});
   }
   onTravelMethodChange(event) {
-
-    console.log(event.target.value);
     this.setState({method: event.target.value})
   }
 
@@ -143,15 +138,15 @@ export default class RoutePlanner extends Component {
       const responseBody = JSON.parse(this.response);
     })
     request.send(JSON.stringify(this.state.user));
+    alert("Your route has been saved")
   }
-
-
 
   newRoute = function(startCoords, endCoords, method, startTown, endTown){
 
     if(this.state.been_routed === true){
     this.state.routing.spliceWaypoints(0, 2);
     this.map.removeControl(this.state.routing)}
+
      this.state.routing = L.Routing.control({
       router: new L.Routing.GraphHopper('3eff14d7-7b89-4050-98ef-f0d72edb928e', {
         urlParameters: {
@@ -166,7 +161,7 @@ export default class RoutePlanner extends Component {
       this.addMarker(startCoords, startTown, "marker");
       this.addMarker(endCoords, endTown, "marker2");
 
-     this.state.been_routed = true;
+     this.setState({been_routed:true})
      this.setState({usersRoute: {name: `${this.state.startTown} to ${this.state.endTown}`, completed: "Not completed"}})
 
     }
@@ -243,14 +238,14 @@ export default class RoutePlanner extends Component {
         <div id="map-box">
           <div id='form-box'>
             <select onChange={this.onTravelMethodChange}>
-              <option value="foot" onClick={this.onTravelMethodChange}>
-                Walking
-              </option>
               <option value="car" onClick={this.onTravelMethodChange}>
                 Driving
               </option>
               <option value="bike" onClick={this.onTravelMethodChange}>
                 Cycling
+              </option>
+              <option value="foot" onClick={this.onTravelMethodChange}>
+                Walking
               </option>
             </select>
             <form onSubmit={this.handleSearchSubmit}>
